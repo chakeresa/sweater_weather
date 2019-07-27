2af16326c9659a48f1b73518fe62c43ad4d465d2
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-27 09:48:46 -0600

    Add GoogleGeocodingApi#geocoding_results

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     parameters = { location_string: params[:location] }
-    lat, long = GoogleGeocodingApi.new(parameters).lat_long
+    # TODO: abstract stuff below into a facade
+    location_hash = GoogleGeocodingApi.new(parameters).geocoding_results
+    formatted_location = location_hash[:results].first[:formatted_address]
+    lat_long_hash = location_hash[:results].first[:geometry][:location]
+    lat = lat_long_hash[:lat]
+    long = lat_long_hash[:lng]
   end
 end
--- app/services/google_geocoding_api.rb
+++ app/services/google_geocoding_api.rb
@@ -1,13 +1,28 @@
 class GoogleGeocodingApi
   attr_reader :location_string
-  
+
   def initialize(parameters =  {})
     @location_string = parameters[:location_string]
   end
 
-  def lat_long
-    require 'pry'; binding.pry
-    @location_string
-    lat, long = 1, 2
+  def geocoding_results
+    uri_path = '/maps/api/geocode/json'
+    location_hash = fetch_data(uri_path, address: @location_string)
+    raise 'Bad Google Maps API key' if location_hash[:error_message]
+    location_hash
+  end
+
+  private
+
+  def conn
+    @conn ||= Faraday.new(:url => 'https://maps.googleapis.com') do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
+    end
+  end
+
+  def fetch_data(uri_path, params = {})
+    response = conn.get uri_path, params
+    JSON.parse response.body, symbolize_names: true
   end
 end
