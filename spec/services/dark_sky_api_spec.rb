5b0689504e4478d46d0ecfc8f3a24d15dcc699e9
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-27 18:06:58 -0600

    Add tests for bad API keys

--- spec/services/dark_sky_api_spec.rb
+++ spec/services/dark_sky_api_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require './app/services/api_service'
 require './app/services/dark_sky_api'
 require 'faraday'
 require './config/application'
@@ -20,7 +21,7 @@ describe DarkSkyApi do
     lat_lng_hash = { lat: 37.8267, lng: -122.4233 }
     service = DarkSkyApi.new(lat_lng_hash)
     result = service.forecast
-
+    
     expect(result[:currently]).to have_key(:summary)
     expect(result[:currently]).to have_key(:icon)
     expect(result[:currently]).to have_key(:temperature)
@@ -28,12 +29,12 @@ describe DarkSkyApi do
     expect(result[:currently]).to have_key(:humidity)
     expect(result[:currently]).to have_key(:visibility)
     expect(result[:currently]).to have_key(:uvIndex)
-
+    
     expect(result[:hourly][:data].count).to be >= 8
     expect(result[:hourly][:data].first).to have_key(:time)
     expect(result[:hourly][:data].first).to have_key(:icon)
     expect(result[:hourly][:data].first).to have_key(:temperature)
-
+    
     expect(result[:daily][:data].count).to be >= 5
     expect(result[:daily][:data].first).to have_key(:time)
     expect(result[:daily][:data].first).to have_key(:icon)
@@ -42,5 +43,14 @@ describe DarkSkyApi do
     expect(result[:daily][:data].first).to have_key(:precipProbability)
     expect(result[:daily][:data].first).to have_key(:temperatureHigh)
     expect(result[:daily][:data].first).to have_key(:temperatureLow)
+  end
+  
+  it '#forecast raises an error if the API response is bad' do
+    lat_lng_hash = { lat: 37.8267, lng: -122.4233 }
+    service = DarkSkyApi.new(lat_lng_hash)
+
+    stub_const('ENV', {'DARK_SKY_API_KEY' => 'blah'})
+
+    expect { service.forecast }.to raise_error('Bad Dark Sky API key')
   end
 end
--- spec/services/google_geocoding_api_spec.rb
+++ spec/services/google_geocoding_api_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require './app/services/api_service'
 require './app/services/google_geocoding_api'
 require 'faraday'
 require './config/application'
@@ -18,10 +19,18 @@ describe GoogleGeocodingApi do
     location_string = 'denver, co'
     service = GoogleGeocodingApi.new({ location_string: location_string })
     result = service.geocoding_results
-
+    
     expect(result).to have_key(:results)
     expect(result[:results].first).to have_key(:formatted_address)
     expect(result[:results].first[:geometry][:location]).to have_key(:lat)
     expect(result[:results].first[:geometry][:location]).to have_key(:lng)
+  end
+  
+  it '#geocoding_results raises an error if the API response is bad' do
+    service = GoogleGeocodingApi.new({ location_string: 'denver, co' })
+
+    stub_const('ENV', {'GOOGLE_MAPS_API_KEY' => 'blah'})
+
+    expect { service.geocoding_results }.to raise_error('Bad Google Maps API key')
   end
 end
