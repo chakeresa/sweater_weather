874105363d22dee7df4368e81cfc36275a45bc73
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-29 09:16:03 -0600

    Add MunchiesIndexFacade skeleton

---
+++ app/facades/munchies_index_facade.rb
@@ -0,0 +1,50 @@
+class MunchiesIndexFacade
+  def initialize(parameters = {})
+    @start = parameters[:start]
+    @end = parameters[:end]
+    @food = parameters[:food]
+  end
+
+  def full_response
+  #   parameters = {
+  #     api_location_hash: api_location_hash,
+  #     forecast_hash: forecast_hash
+  #   }
+  #   {
+  #     meta: Forecast::Metadata.new(parameters).data,
+  #     data: data
+  #   }
+  end
+
+  # def data
+  #   parameters = { forecast_hash: forecast_hash }
+  #   {
+  #     currently: Forecast::Current.new(parameters).data,
+  #     daily: Forecast::Daily.new(parameters).data.first(5),
+  #     hourly: Forecast::Hourly.new(parameters).data.first(8)
+  #   }
+  # end
+
+  # private
+
+  # def google_geocoding_api
+  #   parameters = { location_string: @location_string }
+  #   @google_geocoding_api ||= ApiService::GoogleGeocoding.new(parameters)
+  # end
+  
+  # def api_location_hash
+  #   @api_location_hash ||= google_geocoding_api.geocoding_results
+  # end
+
+  # def lat_lng_hash
+  #   @lat_lng_hash ||= api_location_hash[:results].first[:geometry][:location]
+  # end
+
+  # def dark_sky_api
+  #   @dark_sky_api ||= ApiService::DarkSky.new(lat_lng_hash)
+  # end
+
+  # def forecast_hash
+  #   @forecast_hash ||= dark_sky_api.forecast
+  # end
+end
