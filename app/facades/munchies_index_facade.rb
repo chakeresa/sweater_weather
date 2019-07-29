355de9b2b2b5cce0e706b13b2c27dbf5c25784c7
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-29 10:28:25 -0600

    Add #yelp_api & #restaurants to  MunchiesIndexFacade

--- app/facades/munchies_index_facade.rb
+++ app/facades/munchies_index_facade.rb
@@ -6,17 +6,10 @@ class MunchiesIndexFacade
   end
 
   def full_response
-    # TODO
-    arrival_epoch
-    require 'pry'; binding.pry
-  #   parameters = {
-  #     api_location_hash: api_location_hash,
-  #     forecast_hash: forecast_hash
-  #   }
-  #   {
-  #     meta: Forecast::Metadata.new(parameters).data,
-  #     data: data
-  #   }
+    {
+      meta: { location: @destination },
+      data: { restaurants: restaurants }
+    }
   end
   
   private
@@ -36,5 +29,27 @@ class MunchiesIndexFacade
 
   def arrival_epoch
     Time.now.to_i + duration_in_seconds
+  end
+
+  def yelp_api
+    parameters = { 
+      location: @destination,
+      food_type: @food,
+      epoch: arrival_epoch
+    }
+    @yelp_api ||= ApiService::Yelp.new(parameters)
+  end
+  
+  def api_restaurants_hash
+    @api_restaurants_hash ||= yelp_api.restaurants
+  end
+
+  def restaurants
+    api_restaurants_hash[:businesses].first(3).map do |restaurant|
+      {
+        name: restaurant[:name],
+        address: restaurant[:location][:display_address]
+      }
+    end
   end
 end
