cc3d6ac562387c33899e26227e0ddee19db1b4d0
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-30 11:00:33 -0600

    Setup vcr cassettes

--- spec/services/api_service/dark_sky_spec.rb
+++ spec/services/api_service/dark_sky_spec.rb
@@ -14,44 +14,48 @@ describe ApiService::DarkSky do
   end
   
   it '#forecast returns forecasted data' do
-    lat_lng_hash = { lat: 37.8267, lng: -122.4233 }
-    service = ApiService::DarkSky.new(lat_lng_hash)
-    result = service.forecast
-    
-    expect(result).to have_key(:offset) # UTC offset
-
-    expect(result[:currently]).to have_key(:time)
-    expect(result[:currently]).to have_key(:summary)
-    expect(result[:currently]).to have_key(:icon)
-    expect(result[:currently]).to have_key(:temperature)
-    expect(result[:currently]).to have_key(:apparentTemperature)
-    expect(result[:currently]).to have_key(:humidity)
-    expect(result[:currently]).to have_key(:visibility)
-    expect(result[:currently]).to have_key(:uvIndex)
-    
-    expect(result[:hourly][:data].count).to be >= 8
-    expect(result[:hourly][:data].first).to have_key(:time)
-    expect(result[:hourly][:data].first).to have_key(:icon)
-    expect(result[:hourly][:data].first).to have_key(:temperature)
-    
-    expect(result[:daily][:data].count).to be >= 5
-    expect(result[:daily][:data].first).to have_key(:time)
-    expect(result[:daily][:data].first).to have_key(:icon)
-    expect(result[:daily][:data].first).to have_key(:summary)
-    # TODO: delete precipType from here & serializer?
-    # The API is not including it on Monday
-    # expect(result[:daily][:data].first).to have_key(:precipType)
-    expect(result[:daily][:data].first).to have_key(:precipProbability)
-    expect(result[:daily][:data].first).to have_key(:temperatureHigh)
-    expect(result[:daily][:data].first).to have_key(:temperatureLow)
+    VCR.use_cassette('api_service/dark_sky/forecast', record: :new_episodes) do
+      lat_lng_hash = { lat: 37.8267, lng: -122.4233 }
+      service = ApiService::DarkSky.new(lat_lng_hash)
+      result = service.forecast
+      
+      expect(result).to have_key(:offset) # UTC offset
+      
+      expect(result[:currently]).to have_key(:time)
+      expect(result[:currently]).to have_key(:summary)
+      expect(result[:currently]).to have_key(:icon)
+      expect(result[:currently]).to have_key(:temperature)
+      expect(result[:currently]).to have_key(:apparentTemperature)
+      expect(result[:currently]).to have_key(:humidity)
+      expect(result[:currently]).to have_key(:visibility)
+      expect(result[:currently]).to have_key(:uvIndex)
+      
+      expect(result[:hourly][:data].count).to be >= 8
+      expect(result[:hourly][:data].first).to have_key(:time)
+      expect(result[:hourly][:data].first).to have_key(:icon)
+      expect(result[:hourly][:data].first).to have_key(:temperature)
+      
+      expect(result[:daily][:data].count).to be >= 5
+      expect(result[:daily][:data].first).to have_key(:time)
+      expect(result[:daily][:data].first).to have_key(:icon)
+      expect(result[:daily][:data].first).to have_key(:summary)
+      # TODO: delete precipType from here & serializer?
+      # The API is not including it on Monday
+      # expect(result[:daily][:data].first).to have_key(:precipType)
+      expect(result[:daily][:data].first).to have_key(:precipProbability)
+      expect(result[:daily][:data].first).to have_key(:temperatureHigh)
+      expect(result[:daily][:data].first).to have_key(:temperatureLow)
+    end
   end
   
   it '#forecast raises an error if the API response is bad' do
-    lat_lng_hash = { lat: 37.8267, lng: -122.4233 }
-    service = ApiService::DarkSky.new(lat_lng_hash)
+    VCR.use_cassette('api_service/dark_sky/error_on_bad_api_response', record: :new_episodes) do
+      lat_lng_hash = { lat: 37.8267, lng: -122.4233 }
+      service = ApiService::DarkSky.new(lat_lng_hash)
 
-    stub_const('ENV', {'DARK_SKY_API_KEY' => 'blah'})
+      stub_const('ENV', {'DARK_SKY_API_KEY' => 'blah'})
 
-    expect { service.forecast }.to raise_error('ApiService::DarkSky error: permission denied')
+      expect { service.forecast }.to raise_error('ApiService::DarkSky error: permission denied')
+    end
   end
 end
--- spec/services/api_service/google_spec.rb
+++ spec/services/api_service/google_spec.rb
@@ -18,42 +18,50 @@ describe ApiService::Google do
   end
   
   it '#geocoding_results returns formatted location and lat/long' do
-    location_string = 'denver, co'
-    service = ApiService::Google.new({ location_string: location_string })
-    result = service.geocoding_results
-    
-    expect(result).to have_key(:results)
-    expect(result[:results].first).to have_key(:formatted_address)
-    expect(result[:results].first[:geometry][:location]).to have_key(:lat)
-    expect(result[:results].first[:geometry][:location]).to have_key(:lng)
+    VCR.use_cassette('api_service/google/geocoding_results', record: :new_episodes) do
+      location_string = 'denver, co'
+      service = ApiService::Google.new({ location_string: location_string })
+      result = service.geocoding_results
+      
+      expect(result).to have_key(:results)
+      expect(result[:results].first).to have_key(:formatted_address)
+      expect(result[:results].first[:geometry][:location]).to have_key(:lat)
+      expect(result[:results].first[:geometry][:location]).to have_key(:lng)
+    end
   end
   
   it '#geocoding_results raises an error if the API response is bad' do
-    service = ApiService::Google.new({ location_string: 'denver, co' })
-
-    stub_const('ENV', {'GOOGLE_MAPS_API_KEY' => 'blah'})
-
-    expect { service.geocoding_results }.to raise_error('ApiService::Google error: The provided API key is invalid.')
+    VCR.use_cassette('api_service/google/error_on_bad_geocoding_api_response', record: :new_episodes) do
+      service = ApiService::Google.new({ location_string: 'denver, co' })
+      
+      stub_const('ENV', {'GOOGLE_MAPS_API_KEY' => 'blah'})
+      
+      expect { service.geocoding_results }.to raise_error('ApiService::Google error: The provided API key is invalid.')
+    end
   end
   
   it '#directions returns directions info from origin to destination' do
-    origin = 'denver, co'
-    destination = 'pueblo, co'
-    service = ApiService::Google.new({ origin: origin, destination: destination })
-    result = service.directions
-    
-    expect(result[:routes]).to be_an(Array)
-    expect(result[:routes].first[:legs]).to be_an(Array)
-    expect(result[:routes].first[:legs].first[:duration]).to have_key(:text)
+    VCR.use_cassette('api_service/google/directions', record: :new_episodes) do
+      origin = 'denver, co'
+      destination = 'pueblo, co'
+      service = ApiService::Google.new({ origin: origin, destination: destination })
+      result = service.directions
+      
+      expect(result[:routes]).to be_an(Array)
+      expect(result[:routes].first[:legs]).to be_an(Array)
+      expect(result[:routes].first[:legs].first[:duration]).to have_key(:text)
+    end
   end
   
   it '#directions raises an error if the API response is bad' do
-    origin = 'denver, co'
-    destination = 'pueblo, co'
-    service = ApiService::Google.new({ origin: origin, destination: destination })
-    
-    stub_const('ENV', {'GOOGLE_MAPS_API_KEY' => 'blah'})
+    VCR.use_cassette('api_service/google/error_on_bad_directions_api_response', record: :new_episodes) do
+      origin = 'denver, co'
+      destination = 'pueblo, co'
+      service = ApiService::Google.new({ origin: origin, destination: destination })
+      
+      stub_const('ENV', {'GOOGLE_MAPS_API_KEY' => 'blah'})
 
-    expect { service.directions }.to raise_error('ApiService::Google error: The provided API key is invalid.')
+      expect { service.directions }.to raise_error('ApiService::Google error: The provided API key is invalid.')
+    end
   end
 end
